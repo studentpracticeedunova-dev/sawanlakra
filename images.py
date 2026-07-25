8c7da1bdaee304c4330d9983691891a3348c46ef
studentpracticeedunova-dev <studentpracticeedunova@gmail.com>
2026-07-25 10:30:17 +0530

    pillow

--- images.py
+++ images.py
@@ -1,47 +1,59 @@
 from tkinter import *
-from PIL import Image,ImageTk
+from tkinter import messagebox
 root=Tk()
-root.title("My Project")
-root.geometry("200x100")
-root.config(bg="blue")
+root.title("TODO LIST")
+root.geometry("400x300")
+root.config(bg="white")
 
-Label(root,text="My Project",
-      font=("Arial",18,"bold"),
-      bg="yellow",
-      fg="red").pack(pady=10)
+def addtask():
+    addtask=addtask_entry.get()
+    if addtask!="":
+        add_listbox.insert(END,addtask)
+        addtask_entry.delete(0,END)
+    else:
+        messagebox.showerror("failed")
 
-# 1. Open the original image using Pillow
-original_img1 = Image.open("ChatGPT Image Jul 18, 2026, 04_35_26 PM.png")
-original_img2 = Image.open("Gemini_Generated_Image_dhiwv8dhiwv8dhiw.png")
-original_img3 = Image.open("ChatGPT Image Jul 3, 2026, 06_27_27 PM.png")
+def clear_all():
+    add_listbox.delete(0,END)
 
-# 2. Resize the image to exact pixel dimensions (Width, Height)
-resized_img1 = original_img1.resize((200, 150), Image.Resampling.LANCZOS)
-resized_img2 = original_img2.resize((200, 150), Image.Resampling.LANCZOS)
-resized_img3 = original_img3.resize((200, 150), Image.Resampling.LANCZOS)
+def deletetask():
+    try:
+        selected_addtask_index=add_listbox.curselection()[0]
+        add_listbox.delete(selected_addtask_index)
+    except IndexError:
+        messagebox.showwarning("Warning","you must select a task to delete.")
 
-# 3. Convert the resized Pillow image into a Tkinter-compatible photo image
-tk_img1 = ImageTk.PhotoImage(resized_img1)
-tk_img2 = ImageTk.PhotoImage(resized_img2)
-tk_img3 = ImageTk.PhotoImage(resized_img3)
+Label(root,text="TODO LIST",
+       font=("Arial",20,"bold"),
+       bg="white",
+       fg="red").pack(pady=20)
 
-image_label=Label(root)
-image_label.pack(pady=20)
+Frame1=Frame(root,bg="white")
+Frame1.pack(pady=20)
 
-def show_image1():
-    image_label.config(width=500, height=250)
-    image_label.config(image=tk_img1)
+addtask_entry=Entry(Frame1,font=("Arial",12),width=25)
+addtask_entry.grid(row=0,column=1,padx=10)
 
-def show_image2():
-    image_label.config(width=500, height=250)
-    image_label.config(image=tk_img2)
+Button(Frame1,text="Add Task",
+       font=("Arial",12),
+       bg="yellow",
+       fg="green",
+       command=addtask).grid(row=0,column=0,padx=10)
 
-def show_image3():
-    image_label.config(width=500, height=250)
-    image_label.config(image=tk_img3)
+add_listbox=Listbox(root,font=("Arial",12),width=28,height=10)
+add_listbox.pack(pady=20)
 
-Button(root,text="Image 1",width=15,command=show_image1).pack(pady=5)
-Button(root,text="Image 2",width=15,command=show_image2).pack(pady=5)
-Button(root,text="Image 3",width=15,command=show_image3).pack(pady=5)
-Button(root,text="Exit",width=15,command=root.destroy).pack(pady=5)
+Button(Frame1,text="Clear all",
+       font=("Arial",12),
+       bg="blue",
+       fg="white",
+       width=15,
+       command=clear_all).grid(row=5,column=0,padx=10)
+
+Button(Frame1,text="Delete Task",
+       font=("Arial",12),
+       bg="red",
+       fg="white",
+       width=15,
+       command=deletetask).grid(row=5,column=1,padx=10)
 root.mainloop()
